bfc9e26d65b042b9a35c1b07cd26317db06d18ac
mattmunee <mattmunee@hotmail.com>
2015-08-17 00:01:29 -0400

    Implement Portrait/Landscape interrupt functionality.

--- IntrusionDetectionNode/IntrusionDetectionNode.ino
+++ IntrusionDetectionNode/IntrusionDetectionNode.ino
@@ -9,8 +9,8 @@
 #define SERIAL_BAUD      115200
 #define SERIAL_EN
 #ifdef SERIAL_EN
-#define DEBUG(input)   {Serial.print(input); delay(1);}
-#define DEBUGln(input) {Serial.println(input); delay(1);}
+#define DEBUG(input)   {Serial.print(input); delay(5);}
+#define DEBUGln(input) {Serial.println(input); delay(5);}
 #else
 #define DEBUG(input);
 #define DEBUGln(input);
@@ -35,6 +35,8 @@ ArduinoLED led(LED);
 int numints=0;
 bool motionInterruptCaught = false;
 bool sleepInterruptCaught = false;
+byte intSource;
+accelData data;
 
 void setup()
 {
@@ -53,10 +55,11 @@ void setup()
 
 	// Setup accelerometer
 	accelerometer.init(SCALE_2G, ODR_100);
-	accelerometer.setupFreefallOrMotionDetection(MOTION, XY, 0.63, 0, INT_PIN2);
-	accelerometer.setupFreefallOrMotionDetection(FREEFALL, Y, 0.63, 0, INT_PIN2);
-	accelerometer.setupAutoSleep(ODR_SLEEP_12,LOW_POWER, 0x08, 5.0,INT_PIN1);
-	accelerometer.clearFFMotionInterrupt();
+	//accelerometer.setupFreefallOrMotionDetection(MOTION, XY, 0.63, 0, INT_PIN2);
+	//accelerometer.setupFreefallOrMotionDetection(FREEFALL, Y, 0.63, 0, INT_PIN2);
+	accelerometer.setupPortraitLandscapeDetection(2, INT_PIN2);
+	//accelerometer.setupAutoSleep(ODR_SLEEP_12,LOW_POWER, 0x08, 5.0,INT_PIN2);
+	accelerometer.clearAllInterrupts();
 
 	// Setup Interrupts
 	// Use hardware interrupt 1 for motion interrupt (hardware 0 is tied to radio)
@@ -70,21 +73,37 @@ void setup()
 	PCMSK1 |= 0x01;		//enable indiviual pins on vector PCI1, PCINT8 = bit 0 (PCINT20 = PC0 = ADC0 = D14)
 	sei();
 
+	DEBUG(F("PL_CFG: "));
+	DEBUGln(accelerometer.readRegister(PL_CFG));
+	DEBUG(F("PL_COUNT: "));
+	DEBUGln(accelerometer.readRegister(PL_COUNT));
+	DEBUG(F("CTRL_REG4: "));
+	DEBUGln(accelerometer.readRegister(CTRL_REG4));
+	DEBUG(F("CTRL_REG5: "));
+	DEBUGln(accelerometer.readRegister(CTRL_REG5));
 }
 
 void loop()
 {
 	if (motionInterruptCaught | sleepInterruptCaught){
-		DEBUGln(accelerometer.getInterruptSources());
-		if (motionInterruptCaught)DEBUGln("Motion");
-		if (sleepInterruptCaught)DEBUGln("Sleep");
+		intSource = accelerometer.getInterruptSources();
+		DEBUGln(F("Interrupt Sources:"));
+		if (intSource & 0x01)DEBUGln(F("SRC_DRDY"));
+		if (intSource & 0x04)DEBUGln(F("SRC_FF_MT"));
+		if (intSource & 0x08)DEBUGln(F("SRC_PULSE"));
+		if (intSource & 0x10)DEBUGln(F("SRC_LNDPRT"));
+		if (intSource & 0x20)DEBUGln(F("SRC_TRANS"));
+		if (intSource & 0x80)DEBUGln(F("SRC_ASLP"));
+
+		if (motionInterruptCaught)DEBUGln(F("Motion"));
+		if (sleepInterruptCaught)DEBUGln(F("Sleep"));
 		byte measNum = 0;
 		while (measNum < NUM_MEAS_TX){
 			if (accelerometer.available()){
-				accelerometer.read();
-				payload.data[measNum] = accelerometer.cx*accelerometer.cx
-					+ accelerometer.cy*accelerometer.cy
-					+ accelerometer.cz*accelerometer.cz;
+				data=accelerometer.getData();
+				payload.data[measNum] = data.scaled.x*data.scaled.x
+					+ data.scaled.y*data.scaled.y
+					+ data.scaled.z*data.scaled.z;
 				measNum++;
 			}
 		}
@@ -92,26 +111,26 @@ void loop()
 			DEBUG(payload.data[i]);
 			DEBUG(" ");
 		}
-		DEBUGln("");
-		DEBUGln("");
-		DEBUG("Size Of Payload: ");
+		DEBUGln(F(""));
+		DEBUGln(F(""));
+		DEBUG(F("Size Of Payload: "));
 		DEBUGln(sizeof(Payload));
-		DEBUG("Size Of float: ");
+		DEBUG(F("Size Of float: "));
 		DEBUGln(sizeof(float*));
-		DEBUG("Size Of PiFortNodeType: ");
+		DEBUG(F("Size Of PiFortNodeType: "));
 		DEBUGln(sizeof(PiFortNodeType));
-		DEBUG("Size Of Payload: ");
+		DEBUG(F("Size Of Payload: "));
+		byte payloadSize = payload.payloadSize;
 		DEBUGln(payload.payloadSize);
 		if (radio.sendWithRetry(GATEWAYID, (const void*)(&payload), payload.payloadSize)){
-			DEBUGln("ACK:OK");
+			DEBUGln(F("ACK:OK"));
 		}
 		else{
 			DEBUGln("ACK:BAD");
 		}
-		
-		delay(100);
-		accelerometer.clearFFMotionInterrupt();
-		accelerometer.getSystemMode();
+
+		accelerometer.clearAllInterrupts();
+
 		motionInterruptCaught = false;
 		sleepInterruptCaught = false;
 	}
